6d2167819a5b3963c75055882c0bd9ce55186a53
arbass <13ar.bass@gmail.com>
2024-08-25 14:38:40 +0300

    updated

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { animationGlitchOnHover } from '$utils/animation_glitch-on-hover';
 import { typeTextOnScroll } from '$utils/animation_scramble-text-on-scroll';
+import { webflow_checkerQa } from '$utils/webflow_checker-qa';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
   animationGlitchOnHover();
   typeTextOnScroll();
+  webflow_checkerQa();
 });
