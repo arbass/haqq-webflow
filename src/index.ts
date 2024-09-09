0a8e09b7f47450f354718e54272c09424ab633f7
arbass <13ar.bass@gmail.com>
2024-09-09 11:52:29 +0300

    links

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,13 @@ import { typeTextOnScroll } from '$utils/animation_scramble-text-on-scroll';
 import { dropdownTabMaster_func } from '$utils/dropdown-tab-master_func';
 import { profilePopups_func } from '$utils/profile-popups';
 import { toc_func } from '$utils/shariah-nav-link';
+import { kopytokToolCmsEasy } from '$utils/webflow_kopytok-tool-cms-easy';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
   animationGlitchOnHover();
   typeTextOnScroll();
-  // kopytokToolCmsEasy();
+  kopytokToolCmsEasy();
   dropdownTabMaster_func();
   profilePopups_func();
   toc_func();
