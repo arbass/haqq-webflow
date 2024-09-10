a50ee61c224c9f06cd518c0af16aba6112f90808
arbass <13ar.bass@gmail.com>
2024-09-10 07:03:46 +0300

    removed: glitch

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import { animationGlitchOnHover } from '$utils/animation_glitch-on-hover';
 import { typeTextOnScroll } from '$utils/animation_scramble-text-on-scroll';
 import { dropdownTabMaster_func } from '$utils/dropdown-tab-master_func';
 import { profilePopups_func } from '$utils/profile-popups';
@@ -7,7 +6,6 @@ import { kopytokToolCmsEasy } from '$utils/webflow_kopytok-tool-cms-easy';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  animationGlitchOnHover();
   typeTextOnScroll();
   kopytokToolCmsEasy();
   dropdownTabMaster_func();
