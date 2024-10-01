bdae427fe9a74755bec9854195e5bd72734071de
arbass <13ar.bass@gmail.com>
2024-10-01 23:12:37 +0300

    updated

--- src/utils/profile-popups.ts
+++ src/utils/profile-popups.ts
@@ -8,26 +8,11 @@ export const profilePopups_func = () => {
       const closeBtn = element.querySelector('.prfile-popup-close');
 
       if (trigger && popup && closeBtn) {
-        let scrollPosition = 0;
-
         trigger.addEventListener('click', () => {
-          // Store the current scroll position
-          scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
-
-          // Add class to body to fix position and prevent scrolling
-          document.body.classList.add('body-scroll-lock');
-          document.body.style.top = `-${scrollPosition}px`;
-
           popup.style.display = 'flex';
         });
 
         closeBtn.addEventListener('click', () => {
-          // Remove the class and restore scroll position
-          document.body.classList.remove('body-scroll-lock');
-          document.body.style.top = '';
-
-          window.scrollTo(0, scrollPosition);
-
           popup.style.display = 'none';
         });
       }
