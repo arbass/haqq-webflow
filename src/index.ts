b2217433800e34f2b55491b5d0e4dbec3da47f15
arbass <13ar.bass@gmail.com>
2024-10-08 16:05:35 +0300

    updated: no typing script

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import { academyPage_func } from '$utils/academy-page_func';
-import { typeTextOnScroll } from '$utils/animation_scramble-text-on-scroll';
 import { buttonsBrand_func } from '$utils/buttons-brand_func';
 import { cardHoverOnScroll_func } from '$utils/card-hover-on-scroll';
 import { cardHoverOnScrollRoadmap_func } from '$utils/card-hover-on-scroll-roadmap';
@@ -14,7 +13,6 @@ import { tabsMainProgress_func } from '$utils/tabs-main-progress';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  typeTextOnScroll();
   dropdownTabMaster_func();
   profilePopups_func();
   toc_func();
